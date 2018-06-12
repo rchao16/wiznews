dc1cb2be2f53b8c3290450a9ade98379137e59ab
Raymond Chao <raymond.chao16@gmail.com>
2018-06-12 16:47:56 -0500

    second

--- app.js
+++ app.js
@@ -1,21 +1,65 @@
 const express = require("express");
 const morgan = require('morgan');
-const postBank = require('./postBank')
+const postBank = require('./postBank');
+const staticMiddleware = express.static('./public');
 const app = express();
 
 
 app.use(morgan('dev'));
 
+app.use(staticMiddleware);
+
 app.get("/", (req, res) => {
     let posts = postBank.list();
-      let html = `<html>
-        <ul>
-      ${posts.map(post => `<li>${post.title} by ${post.name}</li>`).join('')}
-        </ul>
-      </html>`;
+      let html = `<!DOCTYPE html>
+      <html>
+      <head>
+        <title>Wizard News</title>
+        <link rel="stylesheet" href="/style.css" />
+      </head>
+      <body>
+        <div class="news-list">
+          <header><img src="/logo.png"/>Wizard News</header>
+          ${posts.map(post => `
+            <div class='news-item'>
+              <p>
+                <span class="news-position">${post.id}. ▲</span>${post.title}
+                <small>(by ${post.name})</small>
+              </p>
+              <small class="news-info">
+                ${post.upvotes} upvotes | ${post.date}
+              </small>
+            </div>`
+          ).join("")}
+        </div>
+      </body>
+    </html>`;
 
       res.send(html);
 });
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  res.send(
+    `<head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <div class = 'news-item'>
+      <h1>
+        ${post.title} (by ${post.name})
+      </h1>
+
+      <p>
+        ${post.content}
+      </p>
+    </div>
+    `
+  );
+});
+
+
 
 const PORT = 1337;
 
