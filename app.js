552f7ea346972ce5935657a09a691af1ef234e6d
Brendan Meyer <brenmeyer2245@gmail.com>
2018-06-12 16:12:46 -0500

    list posts

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const morgan = require('morgan');
+const postBank = require('./postBank')
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use(morgan('dev'));
+
+app.get("/", (req, res) => {
+    let posts = postBank.list();
+      let html = `<html>
+        <ul>
+      ${posts.map(post => `<li>${post.title} by ${post.name}</li>`).join('')}
+        </ul>
+      </html>`;
+
+      res.send(html);
+});
 
 const PORT = 1337;
 
